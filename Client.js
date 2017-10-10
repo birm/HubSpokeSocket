1caafb1d513a9cd5c7fbe1650e16cc2912b36c4d
Ryan Birmingham <birm@rbirm.us>
2017-10-10 13:33:27 -0400

    some neatening

--- Client.js
+++ Client.js
@@ -1,6 +1,5 @@
 // requires socket io client to be included
 
-
 /* Spoke - communicate state information with hub using (this).state = {Value}
  * @constructor
  * @param hub_id - the hub's hash, and this item's base hash
@@ -9,36 +8,35 @@
  * set event callback by {this}.callback = {callback}
  */
 class Spoke {
-    constructor(hub_id, id, hostname) {
-        this.hub_id = hub_id
-        this.id = hub_id + "-" + id;
-        this._state = "";
-        this.socket = io(hostname);
-        this.socket.on('hubEvent', (e) => this.listen(e));
-        this.callback = console.log;
-    }
+  constructor (hub_id, id, hostname) {
+    this.hub_id = hub_id
+    this.id = hub_id + '-' + id
+    this._state = ''
+    this.socket = io(hostname)
+    this.socket.on('hubEvent', (e) => this.listen(e))
+    this.callback = console.log
+  }
 
-    listen(event) {
-        if (event.key == this.hub_id) {
-            this._state = event.value;
-            this.callback(event.value);
-        }
+  listen (event) {
+    if (event.key == this.hub_id) {
+      this._state = event.value
+      this.callback(event.value)
     }
+  }
 
-    set state(value) {
+  set state (value) {
         /* set this hub's state and update hub */
-        this._state = value;
-        this.socket.emit("spokeEvent", {
-            key: this.id,
-            value: value
-        });
-    }
-    get state() {
+    this._state = value
+    this.socket.emit('spokeEvent', {
+      key: this.id,
+      value: value
+    })
+  }
+  get state () {
         /* get parent state */
         // the last hub state is the state we need alyways
-        return this._state;
-    }
-
+    return this._state
+  }
 }
 
 /* Hub - communicate state information with spokes using (this).state = {Value}
@@ -48,46 +46,45 @@ class Spoke {
  * The hub also tracks each spoke's last update using (this).spokes{spokeid}
  */
 class Hub {
-    constructor(hostname) {
-        this._state = "";
+  constructor (hostname) {
+    this._state = ''
         // INSECURE HASH FUNCTION to avoid collision
-        var dt = new Date().toString(),
-            hash = 0;
-        for (var i = 0; i < dt.length; i++) {
-            var char = dt.charCodeAt(i);
-            hash = ((hash << 5) - hash) + char;
-            hash = hash & hash; // Convert to 32bit integer
-            hash = Math.abs(hash);
-        }
-        this.id = hash;
-        this.socket = io(hostname);
-        this.socket.on('spokeEvent', (e) => this.listen(e));
-        this.callback = console.log;
-        this.spokes = {}
+    var dt = new Date().toString(),
+      hash = 0
+    for (var i = 0; i < dt.length; i++) {
+      var char = dt.charCodeAt(i)
+      hash = ((hash << 5) - hash) + char
+      hash = hash & hash // Convert to 32bit integer
+      hash = Math.abs(hash)
     }
+    this.id = hash
+    this.socket = io(hostname)
+    this.socket.on('spokeEvent', (e) => this.listen(e))
+    this.callback = console.log
+    this.spokes = {}
+  }
 
-    listen(event) {
-        if (!(this.id == event.key) & event.key.toString().split("-")[0] == this.id) {
-            this._state = event.value;
+  listen (event) {
+    if (!(this.id == event.key) & event.key.toString().split('-')[0] == this.id) {
+      this._state = event.value
             // use the spoke id as used on construction
-            let spoke = event.key.toString();
-            this.spokes{spoke.substr(spoke.indexOf("-")+1)} = event.value;
+      let spoke = event.key.toString()
+      this.spokes[spoke.substr(spoke.indexOf('-') + 1)] = event.value
             // call the callback
-            this.callback(event.value);
-        }
+      this.callback(event.value)
     }
+  }
 
-    set state(value) {
-        /* set this hub's state and update spokes*/
-        this._state = value;
-        this.socket.emit("hubEvent", {
-            key: this.id,
-            value: value
-        });
-    }
-    get state() {
+  set state (value) {
+        /* set this hub's state and update spokes */
+    this._state = value
+    this.socket.emit('hubEvent', {
+      key: this.id,
+      value: value
+    })
+  }
+  get state () {
         // the state is the last updated of all states
-        return this._state;
-    }
-
+    return this._state
+  }
 }
--- Server.js
+++ Server.js
@@ -1,19 +1,19 @@
-var server = require('http').createServer();
-var io = require('socket.io')(server);
+var server = require('http').createServer()
+var io = require('socket.io')(server)
 
-function Server(){
-  io.on('connection', function(client){
-    client.on('hubEvent', function(data){
-      io.emit("hubEvent", data);
-    });
-    client.on('spokeEvent', function(data){
-      io.emit("spokeEvent", data);
-    });
-    client.on('disconnect', function(){});
-  });
-  server.listen(3001);
+function Server () {
+  io.on('connection', function (client) {
+    client.on('hubEvent', function (data) {
+      io.emit('hubEvent', data)
+    })
+    client.on('spokeEvent', function (data) {
+      io.emit('spokeEvent', data)
+    })
+    client.on('disconnect', function () {})
+  })
+  server.listen(3001)
 }
 
 Server()
 
-module.exports = Server;
+module.exports = Server
